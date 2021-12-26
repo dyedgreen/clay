efe9d2bd187fb4f3255c9c240db3469f8a3fdc92
Tilman Roeder <dyed.green.info@gmail.com>
2021-12-26 17:58:34 +0000

    Add test for bad command groups

--- src/group_test.ts
+++ src/group_test.ts
@@ -1,7 +1,10 @@
 import { Command } from "./command.ts";
 import { CommandGroup } from "./group.ts";
 import { string } from "./types.ts";
-import { assertEquals } from "https://deno.land/std@0.118.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.118.0/testing/asserts.ts";
 
 const cmd = new Command("Test child.").required(string, "test");
 
@@ -15,6 +18,18 @@ Deno.test("basic command groups", () => {
 
   const secondResult = group.parse(["second", "test"]);
   assertEquals(secondResult, { second: { test: "test" } });
+});
+
+Deno.test("basic command group errors", () => {
+  const group = new CommandGroup("A test group.")
+    .subcommand("first", cmd)
+    .subcommand("second", cmd);
+
+  assertThrows(() => group.parse(["bad-key", "test"]));
+  assertThrows(() => group.parse(["FIRST", "test"]));
+  assertThrows(() => group.parse(["Second", "test"]));
+  assertThrows(() => group.parse(["first", "test", "too many"]));
+  assertThrows(() => group.parse(["first"]));
 });
 
 Deno.test("command group help output", () => {
