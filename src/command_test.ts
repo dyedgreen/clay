774c491e398694e7799ef6fdb30c1e6ee0273890
Tilman Roeder <dyed.green.info@gmail.com>
2021-12-26 20:15:49 +0000

    Add further tests for command

--- src/command_test.ts
+++ src/command_test.ts
@@ -5,7 +5,7 @@ import {
   assertThrows,
 } from "https://deno.land/std@0.118.0/testing/asserts.ts";
 
-Deno.test("basic commands", () => {
+Deno.test("basic command usage", () => {
   const cmd = new Command("A test command.")
     .required(string, "firstName")
     .required(string, "name")
@@ -13,6 +13,126 @@ Deno.test("basic commands", () => {
 
   const result = cmd.parse(["Peter", "Parker", "--age", "42"]);
   assertEquals(result, { firstName: "Peter", name: "Parker", age: 42 });
+});
+
+Deno.test("basic command errors", () => {
+  const cmd = new Command("A test command.")
+    .required(string, "name", { flags: ["n", "name"] });
+
+  assertThrows(() => {
+    cmd.parse(["--wrong-flag", "Peter Parker"]);
+  });
+  assertThrows(() => {
+    cmd.parse(["-n"]);
+  });
+});
+
+Deno.test("required arguments work as expected", () => {
+  const cmd = new Command("A test command.")
+    .required(string, "a")
+    .required(string, "b", { flags: ["flag"] });
+
+  assertEquals(cmd.parse(["test", "--flag", "test"]), { a: "test", b: "test" });
+  assertThrows(() => cmd.parse(["test"]));
+  assertThrows(() => cmd.parse(["test", "--flagg", "test"]));
+  assertThrows(() => cmd.parse(["--flag", "test"]));
+  assertThrows(() => cmd.parse(["too", "many", "args"]));
+});
+
+Deno.test("optional arguments work as expected", () => {
+  const cmd = new Command("A test command.")
+    .optional(string, "a")
+    .optional(string, "b", { flags: ["flag"] });
+
+  assertEquals(cmd.parse(["test", "--flag", "test"]), { a: "test", b: "test" });
+  assertEquals(cmd.parse(["test"]), { a: "test", b: null });
+  assertEquals(cmd.parse(["--flag", "test"]), { a: null, b: "test" });
+  assertEquals(cmd.parse([]), { a: null, b: null });
+  assertThrows(() => cmd.parse(["--flag-not-known", "test"]));
+  assertThrows(() => cmd.parse(["too", "many", "args"]));
+});
+
+Deno.test("named arguments work as expected", () => {
+  const cmd = new Command("A test command.")
+    .required(string, "a", { flags: ["a", "long-flag", "third"] });
+
+  assertEquals(cmd.parse(["-a", "test"]), { a: "test" });
+  assertEquals(cmd.parse(["--long-flag", "test"]), { a: "test" });
+  assertEquals(cmd.parse(["--third", "test"]), { a: "test" });
+  assertThrows(() => cmd.parse(["--a", "test"]));
+  assertThrows(() => cmd.parse(["-long-flag", "test"]));
+  assertThrows(() => cmd.parse(["-third", "test"]));
+  assertThrows(() => cmd.parse(["a", "test"]));
+  assertThrows(() => cmd.parse(["long-flag", "test"]));
+  assertThrows(() => cmd.parse(["third", "test"]));
+});
+
+Deno.test("can provide non standard number of dashes", () => {
+  const cmd = new Command("A test command.")
+    .optional(string, "a", { flags: ["-long-single-dash"] })
+    .optional(string, "b", { flags: ["--s"] })
+    .optional(string, "c", { flags: ["---three-dash"] });
+
+  assertEquals(cmd.parse(["-long-single-dash", "test"]), {
+    a: "test",
+    b: null,
+    c: null,
+  });
+  assertThrows(() => cmd.parse(["--long-single-dash", "test"]));
+
+  assertEquals(cmd.parse(["--s", "test"]), {
+    a: null,
+    b: "test",
+    c: null,
+  });
+  assertThrows(() => cmd.parse(["-s", "test"]));
+
+  assertEquals(cmd.parse(["---three-dash", "test"]), {
+    a: null,
+    b: null,
+    c: "test",
+  });
+  assertThrows(() => cmd.parse(["--three-dash", "test"]));
+});
+
+Deno.test("consecutive named arguments overwrite", () => {
+  const cmd = new Command("A test command.")
+    .required(string, "a", { flags: ["a", "long-flag", "third"] });
+
+  assertEquals(cmd.parse(["-a", "one", "-a", "two"]), { a: "two" });
+  assertEquals(
+    cmd.parse(["-a", "one", "--third", "two", "--long-flag", "three"]),
+    { a: "three" },
+  );
+});
+
+Deno.test("flags work as expected", () => {
+  const cmd = new Command("A test command.")
+    .flag("test");
+
+  assertEquals(cmd.parse(["--test"]), { test: true });
+  assertEquals(cmd.parse(["--test", "--test"]), { test: true });
+  assertEquals(cmd.parse([]), { test: false });
+  assertThrows(() => cmd.parse(["--test", "test"]));
+  assertThrows(() => cmd.parse(["-test"]));
+  assertThrows(() => cmd.parse(["test"]));
+});
+
+Deno.test("flag aliases work", () => {
+  const cmd = new Command("A test command.")
+    .flag("test", { aliases: ["t", "alias"] });
+
+  assertEquals(cmd.parse(["--test"]), { test: true });
+  assertEquals(cmd.parse(["-t"]), { test: true });
+  assertEquals(cmd.parse(["--alias"]), { test: true });
+  assertEquals(cmd.parse(["-t", "--alias"]), { test: true });
+  assertEquals(cmd.parse(["--test", "--alias"]), { test: true });
+  assertEquals(cmd.parse(["-t", "--test"]), { test: true });
+  assertEquals(cmd.parse(["-t", "--test", "--alias"]), { test: true });
+  assertEquals(cmd.parse([]), { test: false });
+  assertThrows(() => cmd.parse(["--test", "test"]));
+  assertThrows(() => cmd.parse(["-test"]));
+  assertThrows(() => cmd.parse(["test"]));
 });
 
 Deno.test("command help output", () => {
@@ -80,16 +200,4 @@ OPTIONS:
 \t-o, --other <STRING>              Other option.
 \t--flag, -f, --a-flag              This is a flag.`,
   );
-});
-
-Deno.test("basic command errors", () => {
-  const cmd = new Command("A test command.")
-    .required(string, "name", { flags: ["n", "name"] });
-
-  assertThrows(() => {
-    cmd.parse(["--wrong-flag", "Peter Parker"]);
-  });
-  assertThrows(() => {
-    cmd.parse(["-n"]);
-  });
 });
